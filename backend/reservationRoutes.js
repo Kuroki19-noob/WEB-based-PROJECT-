3e42125c74d3cb4f3f593dcbb214c1b3648f9187
groovyrey <reymartcenteno03@gmail.com>
2026-05-07 13:02:24 +0000

    chore: refactor backend, add validation, and create admin dashboard

--- backend/reservationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const {createReservation} = require('../controllers/reservationController')
-
-const router =express.Router()
-
-router.post('/create-reservation', createReservation)   
-
-
-module.exports = router
